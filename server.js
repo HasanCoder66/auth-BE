f65fce5721520f1291a3cb9b3a41abe6f55f6cbc
hasanCoder66 <hasanashraf874@gmail.com>
2026-05-09 18:58:26 +0500

    changes for be deployment

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import {app} from "./src/app.js"
-import dotenv from "dotenv"
-import { connectDb } from "./src/config/db.js"
+// import dotenv from "dotenv"
+import { connectDb, isConnected } from "./src/config/db.js"
 
-dotenv.config()
-
-const PORT = process.env.PORT || 5500
-
-app.listen(PORT, () => {
-    console.log("server is listenting on port:" + PORT)
-    connectDb()
+// dotenv.config()
+app.use((req, res, next) => {
+    if(!isConnected){
+        connectDb()
+    }
+    next()
 })
+
+
+// const PORT = process.env.PORT || 5500
+
+// app.listen(PORT, () => {
+//     console.log("server is listenting on port:" + PORT)
+//     connectDb()
+// })
+
+export {app}
--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,14 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
+export const isConnected = false;
+
 export const connectDb = async () => {
-    let con = await mongoose.connect(process.env.MONGO_URI)
+    try {
+        let con = await mongoose.connect(process.env.MONGO_URI)
+        isConnected = true
     console.log("DB Connected ==> ",con.connection.host)
+    } catch (error) {
+        console.log("Error in connection to db -->",error)
+    }
 }
